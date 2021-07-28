9eb9b400fc9c5d11c24d1e4b2d90d4c037c408bb
Naweli777 <naweliverma7@gmail.com>
2021-07-28 15:49:59 +0530

    deploy fixing

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -5,7 +5,7 @@ import "./Profile.css";
 const Profile = ({name,phone,email}) => {
     return (
         <div className="Card">
-             <LazyLoad height={500} >
+             <LazyLoad height={10000} >
             <img src={`https://avatars.dicebear.com/api/male/${name}.svg?background=%230000ff`} height="100px" width="100px" alt={name} />
             </LazyLoad>
             <div className="info">
